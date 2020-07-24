103d78283e9798b6e8b5b9e022296f05064dfe1a
Digitman27 <digitman27@gmail.com>
2020-07-24 14:22:06 +0300

    eftia3a to filtrarisma gia ta 10km pou leei stin ekfwnhsh

--- import.js
+++ import.js
@@ -9,10 +9,34 @@ function import_files(x){
 	fr.readAsText(file);
 
 	function receive(e){
-		var newArr = [];
+		var filtered_data=[];
 		let lines = e.target.result;
-		var newArr = JSON.parse(lines);
-		console.log(newArr.name);
+		var json_data_arr = JSON.parse(lines);
+		var i,len;
+		len = json_data_arr.locations.length;
+		console.log(json_data_arr);
+		for(i=0;i<len;i++){
+			var tmp;
+			tmp = json_data_arr.locations[i];
+			var dist = cut_distance(tmp.latitudeE7/1e7, tmp.longitudeE7/1e7);
+			//console.log('dist is:',dist);
+			if(dist <= 10000){ // filtrarw ta dedomena kai pairnw mono auta p einai katw apo 10km
+				console.log(i)
+				filtered_data.push(tmp);
+			}
+		}
+		console.log(filtered_data);
+		console.log('END!');
+	}
+
+	function cut_distance(la,lo){
+		var la_const,lo_const,dist;
+		la_const = 38.230462; // latitude pou to dinei katw sthn ekfwnhsh
+		lo_const = 21.753150; // longitude pou to dinei katw sthn ekfwnhsh
+		// ton tupo ton eida apo edw http://janmatuschek.de/LatitudeLongitudeBoundingCoordinates
+		dist = Math.acos(Math.sin(la) * Math.sin(la_const) + Math.cos(la)*Math.cos(la_const)*Math.cos(lo - lo_const))*10000;
+		return dist;
+
 	}
 
 }
