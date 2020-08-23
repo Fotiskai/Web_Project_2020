79efdabffc8249afbc9452320d80534a7aa25ba3
mike <mixalaras1237@gmail.com>
2020-08-23 11:26:51 +0300

    veltiwsh sxoliasmou

--- distribution.php
+++ distribution.php
@@ -1,25 +1,24 @@
 <?php
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "root";
-	$DB = "web2020";
-  $conn = new mysqli($servername,$username,$password,$DB);
-	if($conn->connect_error){
-		die("Connection Failed: " . $conn->connect_error);
-	}
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$DB = "web2020";
+$conn=mysqli_connect("localhost","root","root","web2020");
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 mysqli_set_charset($conn,'utf8');
 
-
-$sql="SELECT COUNT(*) as total FROM data WHERE type !=''";
+/*--------------Κατανομή πλήθους εγγραφών ανα δραστηριοτήτα-----------*/
+$sql="SELECT COUNT(*) as total FROM data WHERE type !=''"; //πλήθος όλων
 $result=mysqli_query($conn,$sql);
 $result=mysqli_fetch_assoc($result);
 $totalact=$result["total"];
 
-$sql="SELECT type,COUNT(*) as count FROM data WHERE type!='' GROUP BY type";
+$sql="SELECT type,COUNT(*) as count FROM data WHERE type!='' GROUP BY type"; //πλήθος κάθε δραστηριότητας
 $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
-     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
     	$type=$row["type"];
     	$cnt=$row["count"];
@@ -30,16 +29,17 @@ if (mysqli_num_rows($result) > 0) {
     echo "0 results";
 }
 
-
+/*Κατανομή πλήθους εγγραφών ανα χρήστη (πλήθος χρηστών-κουβάς εγγραφών)*/
 $sql="SELECT COUNT(DISTINCT(userid)) as count FROM data ";
 $result=mysqli_query($conn,$sql);
 $result=mysqli_fetch_assoc($result);
 $count=$result["count"];
-$k=ceil(sqrt($count));
-
+$k=ceil(sqrt($count)); // πλήθος κουβάδων (√αριθμός χρηστών)
 $tmp=floor($totalact/$k);
 $start=0;
 $finish=$tmp;
+
+/*------δημιουργία κουβάδων εγγραφών----------*/
 $buckets[0]=$start . "-" . $finish;
 for($i=1;$i<$k;$i++){
    $start=$finish+1;
@@ -51,6 +51,7 @@ $last=($finish+1) ."-". ($finish+($totalact-$finish));
 array_push($buckets,$last);
 }
 
+/*Δημιουργία datapoints*/
 for($i=0;$i<$k;$i++){
 	$key=$buckets[$i];
 	$split=explode('-',$buckets[$i]);
@@ -61,15 +62,15 @@ for($i=0;$i<$k;$i++){
     $u_r_count[$key]=$row["usercount"];
 }
 
-
-for($i=1;$i<=12;$i++){//fetch data from act_timestampMs pou afora tous mhnes
+/*------------------Κατανομή πλήθους εγγραφών ανα μήνα--------------------------------*/
+for($i=1;$i<=12;$i++){
     $sql="SELECT COUNT(*) as monthcount FROM data WHERE MONTH(act_timestampMs)='$i'";
     $res=mysqli_query($conn,$sql);
     $res=mysqli_fetch_assoc($res);
     $m_count[$i]=$res["monthcount"];
 }
 
-$count_per_month=array(//store gia ka8e mhna poses eggrafes exoume
+$count_per_month=array(
 "Ιανουάριος" => $m_count[1],
 "Φεβρουάριος" => $m_count[2],
 "Μάρτιος" => $m_count[3],
@@ -84,15 +85,16 @@ $count_per_month=array(//store gia ka8e mhna poses eggrafes exoume
 "Δεκέμβριος" => $m_count[12],
 );
 
+/*------------------Κατανομή πλήθους εγγραφών ανα ημέρα--------------------------------*/
 $tmp_array=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-for($i=0;$i<7;$i++){ //fetch data from act_timestampMs pou afora tis hmeres
+for($i=0;$i<7;$i++){
     $sql="SELECT COUNT(*) as daycount FROM data WHERE DAYNAME(act_timestampMs)='$tmp_array[$i]'";
     $res=mysqli_query($conn,$sql);
     $res=mysqli_fetch_assoc($res);
     $d_count[$i]=$res["daycount"];
 }
 
-$count_per_day=array(//store gia ka8e mera poses eggrafes exoume
+$count_per_day=array(
 "Δευτέρα" => $d_count[0],
 "Τρίτη" => $d_count[1],
 "Τετάρτη" => $d_count[2],
@@ -102,27 +104,30 @@ $count_per_day=array(//store gia ka8e mera poses eggrafes exoume
 "Κυριακή" => $d_count[6],
 );
 
-for($i=0;$i<=23;$i++){//store ton arim8o twn wrwn
+
+/*------------------Κατανομή πλήθους εγγραφών ανα ώρα--------------------------------*/
+for($i=0;$i<=23;$i++){
     $sql="SELECT COUNT(*) as hourcount FROM data WHERE HOUR(act_timestampMs)='$i'";
     $res=mysqli_query($conn,$sql);
     $res=mysqli_fetch_assoc($res);
     $h_count[$i]=$res["hourcount"];
 }
 
-$sql="SELECT MIN(YEAR(act_timestampMs)) as minumum, MAX(YEAR(act_timestampMs)) as maximum FROM data";//brisoume to range ton hmeromhniwn min,max
+/*----------------------------------Κατανομή πλήθους εγγραφών ανα χρόνο----------------------------------------------------------*/
+$sql="SELECT MIN(YEAR(act_timestampMs)) as minumum, MAX(YEAR(act_timestampMs)) as maximum FROM data";  //εύρεση μέγιστου και ελάχιστου έτους εγγραφών
 $result=mysqli_query($conn,$sql);
 $result=mysqli_fetch_assoc($result);
 $mindate=$result["minumum"];
 $maxdate=$result["maximum"];
 
 for($i=$mindate;$i<=$maxdate;$i++){
-    $sql="SELECT COUNT(*) as yearcount FROM data WHERE YEAR(act_timestampMs)='$i'";//
+    $sql="SELECT COUNT(*) as yearcount FROM data WHERE YEAR(act_timestampMs)='$i'";
     $res=mysqli_query($conn,$sql);
     $res=mysqli_fetch_assoc($res);
     $y_count[$i]=$res["yearcount"];
 }
 
-$r = array($dataPoints,$u_r_count,$count_per_month,$count_per_day,$h_count,$y_count);//store all the data in a single array;
+$r = array($dataPoints,$u_r_count,$count_per_month,$count_per_day,$h_count,$y_count);
 echo json_encode($r);
 
 mysqli_close($conn);
